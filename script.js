cc99ccbb8f704ae3f6f8bc89c476a6f8dbfb042c
robrtruns <robrtjaxon@gmail.com>
2019-12-20 16:47:53 -0600

    script.js starting functions

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 var APIkey= "7035bbd87f29595c295712a11e5669aa";
+$(document).ready(function () {
+    
+    
+
+    function show(data) {
+
+        
+        return "<h2>" + data.name + moment().format(' (MM/DD/YYYY)') + "</h2>" +
+            `
+    <p><strong>Temperature</strong>: ${data.main.temp} &#8457</p> 
+    <p><strong>Humidity</strong>: ${data.main.humidity}%</p>
+    <p><strong>Wind Speed</strong>: ${data.wind.speed} MPH</p>
+    <p><strong>UV Index</strong>:${data.coord.lat, data.coord.lon}</p>
+    `
+    
+    
