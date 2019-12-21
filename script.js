ad5fdae38947c9725a6624ca92bd42ffd9258c2c
robrtruns <robrtjaxon@gmail.com>
2019-12-20 19:18:27 -0600

    METHODS

--- script.js
+++ script.js
@@ -34,4 +34,97 @@ $(document).ready(function () {
         var currentForecast = [];
         for (var i = 0; i < forecast.length; i++) {
 
-            var currentObject = forecast[i]
+            var currentObject = forecast[i];
+            
+            var dt_time = currentObject.dt_txt.split(' ')[1] 
+            
+            if (dt_time === "12:00:00") {
+                // currentObject.main ... time, icon, temp, humidity
+                var main = currentObject.main;
+                // Store each of these in variables
+                var temp = main.temp; 
+                var humidity = main.humidity;
+                var date = moment(currentObject.dt_txt).format('l'); 
+                var icon = currentObject.weather[0].icon;
+                
+                console.log('ICON',currentObject.weather[0].icon);
+                var iconurl = "https://openweathermap.org/img/w/" + icon + ".png";
+
+                let htmlTemplate = `
+            <div class="col-sm currentCondition">
+            <div class="card">
+                <div class="card-body 5-day">
+                    <p><strong>${date}</strong></p>
+                    <div><img src=${iconurl} /></div>
+                    <p>Temp: ${temp} &#8457</p>
+                    <p>Humidity: ${humidity}%</p>
+                </div>
+            </div> 
+        </div>`;
+                currentForecast.push(htmlTemplate);
+            }
+
+        }
+        $("#5-day-forecast").html(currentForecast.join(''));
+
+    }
+
+    //METHODS
+
+    var stored = localStorage.getItem("cityList")
+    if (stored) {
+        cityList = JSON.parse(stored);
+    } else {
+        cityList = [];
+    }
+    
+    $('#submitCity').click(function (event) {
+        // test submit button
+        console.log("submited");
+        event.preventDefault();
+        var city = $('#city').val();
+        var lat = 0;
+        var lon = 0;
+        // push city to cityList array
+        cityList.push(city);
+       
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+        displayCities(cityList);
+        if (city != '') {
+            $.ajax({
+                url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=7035bbd87f29595c295712a11e5669aa",
+                type: "GET",
+                success: function (data) {
+                    console.log("WEATHER", data);
+                    var display = show(data);
+                    $("#show").html(display);
+                }
+              
+            });    
+
+
+            $.ajax({
+                url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + "&APPID=7035bbd87f29595c295712a11e5669aa",
+                type: "GET",
+                success: function (data) {
+                    showForecast(data);
+                }
+            });
+
+          
+            $.ajax({
+                url:`https://api.openweathermap.org/data/2.5/uvi?appid=7035bbd87f29595c295712a11e5669aa=${lat}&lon=${lon}`,
+                type: "GET",
+                sucess: function (uvDisplay) {
+                   console.log("uvDisplay", uvDisplay);
+                
+                 }
+            });
+
+        } else {
+            $('#error').html('Please insert a city name:');
+        }
+    });
+
+        displayCities(cityList);
+    });
